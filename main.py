2e40168f2ae76ae550e7f2265e4cf0238188b640
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-17 13:40:56 -0600

    Add gravity effects and the jumping mechanic to the game

--- main.py
+++ main.py
@@ -36,8 +36,25 @@ class Bird(pygame.sprite.Sprite):
     self.image = self.images[self.index]
     self.rect = self.image.get_rect()
     self.rect.center = [x, y]
+    self.vel = 0
+    self.clicked = False
 
   def update(self):
+
+    # gravity
+    self.vel += 0.5
+    if self.vel > 8:
+      self.vel = 8
+    if self.rect.bottom < 768:
+      self.rect.y += int(self.vel)
+
+    # jump
+    if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+      self.clicked = True
+      self.vel = -10
+    if pygame.mouse.get_pressed()[0] == 0:
+      self.clicked = False
+
     # handle the animation for the bird
     self.counter += 1
     flap_cooldown = 5
