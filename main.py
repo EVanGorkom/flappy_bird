2553e3aad1961b7ddfdc5804512432caf40998d6
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-17 16:41:10 -0600

    Revert "Finish the basic functionality of the game"
    This reverts commit 70e78cf23a67c6e6b3125bfb32b80c35c9b9b791.

--- main.py
+++ main.py
@@ -34,20 +34,12 @@ pass_pipe = False
 # load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
-button_img = pygame.image.load('img/restart.png')
 
 
 def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
-
-def reset_game():
-  pipe_group.empty()
-  flappy.rect.x = 100
-  flappy.rect.y = int(screen_height / 2)
-  score = 0
-  return 0
 
 # create the bird object using sprite and add the three different images.
 class Bird(pygame.sprite.Sprite):
@@ -118,29 +110,6 @@ class Pipe(pygame.sprite.Sprite):
         self.kill()
 
 
-class Button():
-  def __init__(self, x, y, image):
-    self.image = image
-    self.rect = self.image.get_rect()
-    self.rect.topleft = (x, y)
-    
-  def draw(self):
-
-    action = False
-
-    # get mouse position
-    pos = pygame.mouse.get_pos()
-
-    # check if mouse is over button
-    if self.rect.collidepoint(pos):
-      if pygame.mouse.get_pressed()[0] == 1:
-        action = True
-
-    # draw button
-    screen.blit(self.image, (self.rect.x, self.rect.y))
-
-    return action
-
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
 
@@ -151,8 +120,6 @@ flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
 
 
-# create restart button instance
-button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
 
 
 # This `run` variable should be consistantly running so that our screen will persist.
@@ -216,12 +183,6 @@ while run:
       ground_scroll = 0
 
     pipe_group.update()
-
-  # check for game over and reset
-  if game_over == True:
-    if button.draw() == True:
-      game_over = False
-      score = reset_game()
 
   # This creates the 'exit' game and allows us to close the game.
   for event in pygame.event.get():
