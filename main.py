59e37787ef325a0ee1a22344a544a5622b280c6c
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-17 13:29:48 -0600

    Add the bird, its animations, and all other images for the project to the img folder

--- main.py
+++ main.py
@@ -19,9 +19,43 @@ fps = 50
 ground_scroll = 0
 scroll_speed = 4
 
-#load images
+# load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+
+# create the bird object using sprite and add the three different images.
+class Bird(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.images =[]
+    self.index = 0
+    self.counter = 0
+    for num in range(1, 4):
+      img = pygame.image.load(f'img/bird{num}.png')
+      self.images.append(img)
+    self.image = self.images[self.index]
+    self.rect = self.image.get_rect()
+    self.rect.center = [x, y]
+
+  def update(self):
+    # handle the animation for the bird
+    self.counter += 1
+    flap_cooldown = 5
+    if self.counter > flap_cooldown:
+      self.counter = 0
+      self.index += 1
+      if self.index >= len(self.images):
+        self.index = 0
+    self.image =  self.images[self.index]
+
+bird_group = pygame.sprite.Group()
+
+# creates the placement of the object
+flappy = Bird(100, int(screen_height / 2))
+
+# makes the connection between the object and the group.
+bird_group.add(flappy)
+
 
 # This `run` variable should be consistantly running so that our screen will persist.
 run = True
@@ -33,6 +67,10 @@ while run:
 
   # display the background image on the screen.
   screen.blit(bg,(0,0))
+
+  # adding the bird to the game
+  bird_group.draw(screen)
+  bird_group.update()
 
   #draw and scroll the ground. This code here changes the code such that the image reset once it exceeds the first diagonal slash, using the timing from the clock feature to give the impression of the ground scrolling endlessly.
   screen.blit(ground_img, (ground_scroll, 768))
